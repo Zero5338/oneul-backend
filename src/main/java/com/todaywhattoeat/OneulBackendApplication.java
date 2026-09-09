e20dc48eac2bd696350c54b155ef85eb54216b65
Zero5338 <a78559@naver.com>
2026-09-09 15:55:04 +0900

    first commit

---
+++ src/main/java/com/todaywhattoeat/OneulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todaywhattoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulBackendApplication.class, args);
+	}
+
+}
